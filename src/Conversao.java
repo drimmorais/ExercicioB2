898294c09de168ed467e7e7f31d725d32f2f94d4
Dri Morais <Adrielle>
2020-03-08 17:47:07 -0300

    primeiro commit

---
+++ src/Conversao.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Conversao {
+	
+	public static void main(String[] args) {
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Entre com o valor do produto: xx,xx: ");
+		double valor = entrada.nextDouble();
+		int valor2 = (int)valor;
+		
+		int valor3 = 10;
+		double valor4 = (double) valor3;
+		
+		
+	}
+}
